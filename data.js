dc67518460faa1ff198d70bf10e2ceb813c7afbc
armenia202 <73358878+armenia202@users.noreply.github.com>
2026-03-21 14:27:39 +0400

    Update athlete images in data.js

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ const athletes = [
     name: "Արման Ծառուկյան",
     sport: "MMA",
     stats: { power: 88, stamina: 95, technique: 92, speed: 90, experience: 82 },
-    image: "images/IMG_20260321_135339.jpg",
+    image: "images/IMG_20260321_135710.jpg",
     bio: "UFC-ի թեթև քաշային կարգի թիվ 1 հավակնորդ:"
   },
   {
@@ -12,7 +12,7 @@ const athletes = [
     name: "Արթուր Ալեքսանյան",
     sport: "Ըմբշամարտ",
     stats: { power: 98, stamina: 92, technique: 96, speed: 78, experience: 100 },
-    image: "images/",
+    image: "images/IMG_20260321_135339.jpg",
     bio: "Օլիմպիական խաղերի ոսկե, արծաթե և բրոնզե մեդալակիր:"
   },
   {
@@ -20,7 +20,7 @@ const athletes = [
     name: "Հենրիկ Մխիթարյան",
     sport: "Ֆուտբոլ",
     stats: { power: 72, stamina: 88, technique: 98, speed: 85, experience: 95 },
-    image: "images/",
+    image: "images/IMG_20260321_135925.jpg",
     bio: "Հայաստանի հավաքականի լավագույն ռմբարկու, Եվրոպա Լիգայի հաղթող:"
   },
   {
@@ -28,7 +28,7 @@ const athletes = [
     name: "Վարազդատ Լալայան",
     sport: "Ծանրամարտ",
     stats: { power: 100, stamina: 65, technique: 88, speed: 72, experience: 85 },
-    image: "images/",
+    image: "images/IMG_20260321_140028.jpg",
     bio: "Աշխարհի և Եվրոպայի առաջնությունների բազմակի մեդալակիր:"
   },
   {
@@ -36,7 +36,7 @@ const athletes = [
     name: "Լևոն Արոնյան",
     sport: "Շախմատ",
     stats: { power: 40, stamina: 80, technique: 100, speed: 60, experience: 98 },
-    image: "images/",
+    image: "images/IMG_20260321_140328.jpg",
     bio: "Շախմատի աշխարհի գավաթակիր, եռակի օլիմպիական չեմպիոն:"
   },
   {
@@ -44,7 +44,7 @@ const athletes = [
     name: "Սիմոն Մարտիրոսյան",
     sport: "Ծանրամարտ",
     stats: { power: 99, stamina: 70, technique: 92, speed: 75, experience: 94 },
-    image: "images/",
+    image: "images/IMG_20260321_140452.jpg",
     bio: "Աշխարհի կրկնակի չեմպիոն և օլիմպիական փոխչեմպիոն:"
   },
   {
@@ -52,7 +52,7 @@ const athletes = [
     name: "Արսեն Ջուլֆալակյան",
     sport: "Ըմբշամարտ",
     stats: { power: 90, stamina: 94, technique: 94, speed: 82, experience: 96 },
-    image: "images/",
+    image: "images/IMG_20260321_140656.jpg",
     bio: "Աշխարհի և Եվրոպայի չեմպիոն, օլիմպիական արծաթե մեդալակիր:"
   },
   {
@@ -60,7 +60,7 @@ const athletes = [
     name: "Էդուարդ Սպերցյան",
     sport: "Ֆուտբոլ",
     stats: { power: 75, stamina: 85, technique: 92, speed: 88, experience: 75 },
-    image: "images/",
+    image: "images/IMG_20260321_140749.jpg",
     bio: "Հայաստանի հավաքականի և «Կրասնոդարի» առաջատար կիսապաշտպան:"
   }
 ];
