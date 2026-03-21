1c776d15ac5bafb6528d85ec110032bfd9630ce8
armenia202 <73358878+armenia202@users.noreply.github.com>
2026-03-21 14:01:48 +0400

    Create data.js with athlete information
    Added a list of athletes with their details including name, sport, stats, image, and bio.

---
+++ data.js
@@ -0,0 +1,67 @@
+const athletes = [
+  {
+    id: "arman-tsarukyan",
+    name: "Արման Ծառուկյան",
+    sport: "MMA",
+    stats: { power: 88, stamina: 95, technique: 92, speed: 90, experience: 82 },
+    image: "https://example.com/arman.jpg",
+    bio: "UFC-ի թեթև քաշային կարգի թիվ 1 հավակնորդ:"
+  },
+  {
+    id: "artur-aleksanyan",
+    name: "Արթուր Ալեքսանյան",
+    sport: "Ըմբշամարտ",
+    stats: { power: 98, stamina: 92, technique: 96, speed: 78, experience: 100 },
+    image: "https://example.com/artur.jpg",
+    bio: "Օլիմպիական խաղերի ոսկե, արծաթե և բրոնզե մեդալակիր:"
+  },
+  {
+    id: "henrikh-mkhitaryan",
+    name: "Հենրիկ Մխիթարյան",
+    sport: "Ֆուտբոլ",
+    stats: { power: 72, stamina: 88, technique: 98, speed: 85, experience: 95 },
+    image: "https://example.com/heno.jpg",
+    bio: "Հայաստանի հավաքականի լավագույն ռմբարկու, Եվրոպա Լիգայի հաղթող:"
+  },
+  {
+    id: "varazdat-lalayan",
+    name: "Վարազդատ Լալայան",
+    sport: "Ծանրամարտ",
+    stats: { power: 100, stamina: 65, technique: 88, speed: 72, experience: 85 },
+    image: "https://example.com/varazdat.jpg",
+    bio: "Աշխարհի և Եվրոպայի առաջնությունների բազմակի մեդալակիր:"
+  },
+  {
+    id: "levon-aronian",
+    name: "Լևոն Արոնյան",
+    sport: "Շախմատ",
+    stats: { power: 40, stamina: 80, technique: 100, speed: 60, experience: 98 },
+    image: "https://example.com/levon.jpg",
+    bio: "Շախմատի աշխարհի գավաթակիր, եռակի օլիմպիական չեմպիոն:"
+  },
+  {
+    id: "simon-martirosyan",
+    name: "Սիմոն Մարտիրոսյան",
+    sport: "Ծանրամարտ",
+    stats: { power: 99, stamina: 70, technique: 92, speed: 75, experience: 94 },
+    image: "https://example.com/simon.jpg",
+    bio: "Աշխարհի կրկնակի չեմպիոն և օլիմպիական փոխչեմպիոն:"
+  },
+  {
+    id: "arsen-julfalakyan",
+    name: "Արսեն Ջուլֆալակյան",
+    sport: "Ըմբշամարտ",
+    stats: { power: 90, stamina: 94, technique: 94, speed: 82, experience: 96 },
+    image: "https://example.com/arsen.jpg",
+    bio: "Աշխարհի և Եվրոպայի չեմպիոն, օլիմպիական արծաթե մեդալակիր:"
+  },
+  {
+    id: "eduard-spertsyan",
+    name: "Էդուարդ Սպերցյան",
+    sport: "Ֆուտբոլ",
+    stats: { power: 75, stamina: 85, technique: 92, speed: 88, experience: 75 },
+    image: "https://example.com/spertsyan.jpg",
+    bio: "Հայաստանի հավաքականի և «Կրասնոդարի» առաջատար կիսապաշտպան:"
+  }
+];
+
